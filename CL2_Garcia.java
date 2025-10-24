4a89d5a04c96e6446ea7d0a6d9e653072447ddac
Saul Garcia <garcialuevanosaul@gmail.com>
2025-10-23 19:16:17 -0600

    CL2_Garcia.java

--- CL2_Garcia.java
+++ CL2_Garcia.java
@@ -1,14 +1,3 @@
-/* Saul Garcia
-[CS1101] Comprehensive Lab 2
-This work is to be done individually. It is not permitted to.
-share, reproduce, or alter any part of this assignment for any
-purpose. Students are not permitted to share code, upload
-this assignment online in any form, or view/receive/
-modifying code written by anyone else. This assignment is part.
-of an academic course at The University of Texas at El Paso and
-a grade will be assigned for the work produced individually by
-the student.
-*/
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -206,3 +195,4 @@ public class CL2_Garcia{
     }
    
 }
+
